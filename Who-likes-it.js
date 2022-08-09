1acb3ffe703aad3247fb7e43fe29bb73be14a3fd
allyson_s <allysonsmithstudio@gmail.com>
2022-08-08 20:54:37 -0400

    6kyu practice

---
+++ Who-likes-it.js
@@ -0,0 +1,81 @@
+/*
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+*/
+
+function likes(names) {
+  // need to know names array length
+
+  if (names.length === 0) {
+    //if empty array
+    return "no one likes this";
+  } else if (names.length === 1) {
+    //if array of 1
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    //if array of 2
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    //if array of 3
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    //4 or more
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+//another solution with switch case:
+
+function likes(names) {
+  names = names || [];
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+      break;
+    case 1:
+      return names[0] + " likes this";
+      break;
+    case 2:
+      return names[0] + " and " + names[1] + " like this";
+      break;
+    case 3:
+      return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+      break;
+    default:
+      return (
+        names[0] +
+        ", " +
+        names[1] +
+        " and " +
+        (names.length - 2) +
+        " others like this"
+      );
+  }
+}
+
+//or using template literals
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return `no one likes this`;
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
+  }
+}
