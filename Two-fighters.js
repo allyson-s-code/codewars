07e1a2e6d776c9837219beefb62840e060508a03
allyson_s <allysonsmithstudio@gmail.com>
2023-02-01 20:58:04 -0500

    7Kyu fighters

---
+++ Two-fighters.js
@@ -0,0 +1,26 @@
+/*
+Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+*/
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  //figure out how many turns until death
+  const fighter1Down = Math.ceil(fighter1.health / fighter2.damagePerAttack);
+  const fighter2Down = Math.ceil(fighter2.health / fighter1.damagePerAttack);
+
+  if (fighter1Down > fighter2Down) {
+    return fighter1.name;
+  } else if (fighter2Down > fighter1Down) {
+    return fighter2.name;
+    //if equal return first attacker
+  } else {
+    return firstAttacker;
+  }
+}
