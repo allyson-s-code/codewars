3ecf59b6c2184046007c8c471902fee966b4ec6b
allyson_s <allysonsmithstudio@gmail.com>
2023-05-16 21:17:27 -0400

    8kyu area of square using arc

---
+++ Area-Square.js
@@ -0,0 +1,12 @@
+/*
+Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+Graph
+
+Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+*/
+
+function squareArea(A) {
+  let radius = (2 * A) / Math.PI;
+  return Number(Math.pow(radius, 2).toFixed(2));
+}
