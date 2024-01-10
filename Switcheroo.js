10a9209679d10535d4ec799fbbead349898730b0
allysonsmith <allysonsmithstudio@gmail.com>
2024-01-10 18:13:20 -0500

    Switcheroo 7kyu solution 1

---
+++ Switcheroo.js
@@ -0,0 +1,17 @@
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+*/
+
+function switcheroo(x){
+    const newArray = [];
+    
+    for (let i = 0; i < x.length; i ++) {
+      if (x[i] === "a") {
+        newArray.push("b");
+      } else if (x[i] === "b") {
+        newArray.push("a");
+        } else {
+          newArray.push(x[i])}
+    } 
+    return newArray.join("");
+  }
